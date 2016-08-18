4fa27c42c0d8ee6de9914138aec88f1caece5db7
Ryuichiroh Ikeuchi <an94_49@yahoo.co.jp>
2016-08-18 14:16:14 +0900

    [modified] pgm.hと9270-2chi.cの修正
    すべての課題ファイルの関数をpgm.hにまとめた。

--- pgm.h
+++ pgm.h
@@ -1,12 +1,13 @@
+#ifndef	_PGM_H_
+#define	_PGM_H_
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
 #include <ctype.h>
 
-
-typedef struct
-{
+typedef struct {
     unsigned char magic_number;
     unsigned int width;
     unsigned int height;
@@ -14,15 +15,17 @@ typedef struct
     unsigned char **data;
 } pgm_t;
 
-
 FILE *open_file(FILE *fp, char *file_name, char *mode);
 int to_i(char *arg, int start, int end);
 unsigned char *to_c(unsigned char *string, int num, int start, int digit);
 int get_header_size(char *file_name);
 int calc_header_size(pgm_t pgm);
 pgm_t set_pgm(pgm_t pgm, char *header);
-pgm_t set_data(pgm_t pgm, unsigned char *buffer_data, unsigned int data_size);
+pgm_t set_data(pgm_t pgm, unsigned char *buffer_data, int data_size);
 unsigned char *create_header(unsigned char *string, pgm_t pgm);
 unsigned char *create_data(unsigned char *string, pgm_t pgm);
 pgm_t read_pgm(char *file_name);
 void write_pgm(char *file_name, pgm_t pgm);
+
+#endif
+
